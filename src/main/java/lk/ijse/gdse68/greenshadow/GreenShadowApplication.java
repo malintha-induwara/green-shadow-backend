091d3d5950675f28fce9e134261797a97abf73f5
Induwara <induwara2k@gmail.com>
2024-10-30 12:10:39 +0530

    chore: initialize the project

---
+++ src/main/java/lk/ijse/gdse68/greenshadow/GreenShadowApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse68.greenshadow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShadowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenShadowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/gdse68/greenshadow/GreenShadowApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse68.greenshadow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
